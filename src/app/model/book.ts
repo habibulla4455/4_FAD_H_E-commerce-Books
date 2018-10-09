3c307f5e9b801bec286101093f0bd12a41ca23ad
K P <kamilpszczola@gmail.com>
2018-10-09 14:11:57 +0200

    Book model update, single page code refactoring
    Simple changes were added, which will be necessary to determine which
    user added specific book to the system.

--- src/app/model/book.ts
+++ src/app/model/book.ts
@@ -2,11 +2,12 @@ export interface Book {
   author: string;
   category: string;
   created: string;
+  descriptionDetail: Array<string>;
   descriptionMain: string;
-  descriptionDetail: string;
   imageUrl: string;
   key: string;
   name: string;
   publisher: string;
   released: string;
+  userId: string;
 }

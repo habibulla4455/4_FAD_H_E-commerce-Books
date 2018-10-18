e1526f1414b2f07df9f68b85f30b5a72ff6bd041
K P <kamilpszczola@gmail.com>
2018-10-18 11:40:30 +0200

    Icon 'on the reading list' notifiaction added.
    Icon was added on the sites which display books:
    -booklist,
    -category display,
    -user books,
    -single book page,
    -search results.
    
    Components need to extract reading list from the UserReadList service.
    Funcitons were created to search for the bookid on user readlist.

--- src/app/components/book-cat-container/book-cat-container.component.ts
+++ src/app/components/book-cat-container/book-cat-container.component.ts
@@ -1,6 +1,9 @@
 import {Component, ElementRef, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
 import {Book} from '../../model/book';
 import {Router} from '@angular/router';
+import {ReadingListPos} from '../../model/readingListPos';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {UserReadListsService} from '../../services/user-read-lists.service';
 
 @Component({
   selector: 'app-book-cat-container',
@@ -17,16 +20,46 @@ export class BookCatContainerComponent implements OnInit, OnChanges {
   bookListPos = 0;
   leftBttnVisible = 0;
   rightBttonVisible = 0;
+  userListSubscription;
+  userReadingList: Array<ReadingListPos>;
 
   @ViewChild('listContainer') listContainer: ElementRef;
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private angularFireAuth: AngularFireAuth,
+    private userReadListService: UserReadListsService,
+  ) {
   }
 
   ngOnInit() {
     if (this.bookListPos + this.bookListWidth > this.listContainer.nativeElement.clientWidth) {
       this.rightBttonVisible = 1;
     }
+    this.angularFireAuth.authState.subscribe(user => {
+      if (user) {
+        this.userListSubscription = this.userReadListService.userReadList.subscribe(list => {
+          this.userReadingList = list;
+        });
+      } else {
+        if (this.userListSubscription) {
+          this.userListSubscription.unsubscribe();
+          this.userReadingList = [];
+        }
+      }
+    });
+  }
+
+  checkIfOnTheList(bookId) {
+    let check = false;
+    if(this.userReadingList) {
+      this.userReadingList.forEach(element => {
+        if (element.bookId === bookId) {
+          check = true;
+        }
+      });
+    }
+    return check;
   }
 
   ngOnChanges(changes) {
--- src/app/components/book-pos/book-pos.component.ts
+++ src/app/components/book-pos/book-pos.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Book} from '../../model/book';
 import {Router} from '@angular/router';
+import {ReadingListPos} from '../../model/readingListPos';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {UserReadListsService} from '../../services/user-read-lists.service';
 
 @Component({
   selector: 'app-book-pos',
@@ -11,10 +14,41 @@ export class BookPosComponent implements OnInit {
 
   @Input() booksList: Array<Book>;
 
-  constructor(private router: Router) {
+  userListSubscription;
+  userReadingList: Array<ReadingListPos>;
+
+  constructor(
+    private router: Router,
+    private angularFireAuth: AngularFireAuth,
+    private userReadListService: UserReadListsService,
+  ) {
   }
 
   ngOnInit() {
+    this.angularFireAuth.authState.subscribe(user => {
+      if (user) {
+        this.userListSubscription = this.userReadListService.userReadList.subscribe(list => {
+          this.userReadingList = list;
+        });
+      } else {
+        if (this.userListSubscription) {
+          this.userListSubscription.unsubscribe();
+          this.userReadingList = [];
+        }
+      }
+    });
+  }
+
+  checkIfOnTheList(bookId) {
+    let check = false;
+    if(this.userReadingList) {
+      this.userReadingList.forEach(element => {
+        if (element.bookId === bookId) {
+          check = true;
+        }
+      });
+    }
+    return check;
   }
 
   redirect(bookId) {

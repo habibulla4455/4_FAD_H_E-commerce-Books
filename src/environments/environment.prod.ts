c3660e5df7a240208e0742e609e1cdb4f6b23aa3
habibulla4455 <habibulla4455@gmail.com>
2019-03-16 21:46:14 +0530

    hello

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyA-VqJ5gtPYz7zo9DWeQQwNY-KUxkyjicQ",
+    authDomain: "shappingcart.firebaseapp.com",
+    databaseURL: "https://shappingcart.firebaseio.com",
+    projectId: "shappingcart",
+    storageBucket: "shappingcart.appspot.com",
+    messagingSenderId: "398108500447"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyA-VqJ5gtPYz7zo9DWeQQwNY-KUxkyjicQ",
+    authDomain: "shappingcart.firebaseapp.com",
+    databaseURL: "https://shappingcart.firebaseio.com",
+    projectId: "shappingcart",
+    storageBucket: "shappingcart.appspot.com",
+    messagingSenderId: "398108500447"
+  }
 };
 
 /*
